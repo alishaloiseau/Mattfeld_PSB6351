fad63cff9dc5b0d81b13aca4989e596421eaeba4
Aaron Mattfeld <amattfeld@gmail.com>
2022-09-26 05:54:44 -0400

    initial hw1 commit

--- code/conversion/psb6351_convertdicoms.sh
+++ code/conversion/psb6351_convertdicoms.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #SBATCH -J psb6351_dcm_convert
-#SBATCH -o /scratch/madlab/Mattfeld_PSB6351/crash/dcm_convert_out
-#SBATCH -e /scratch/madlab/Mattfeld_PSB6351/crash/dcm_convert_err
-#SBATCH --qos pq_madlab
-#SBATCH --account iacc_madlab
-#SBATCH --partition IB_40C_512G
+#SBATCH -o /scratch/classroom/psb6351/amattfel/crash/dcm_convert_out
+#SBATCH -e /scratch/classroom/psb6351/amattfel/crash/dcm_convert_err
+#SBATCH --qos pq_psb6351
+#SBATCH --account acc_psb6351
+#SBATCH --partition classroom
 
 heudiconv -d '/home/amattfel/Mattfeld_PSB6351/sourcedata/Mattfeld_REVL-000-vCAT-{subject}-S1/*/*/*/*/*/*' -s 021 -c none -f convertall -o /home/amattfel/Mattfeld_PSB6351/dset
 
