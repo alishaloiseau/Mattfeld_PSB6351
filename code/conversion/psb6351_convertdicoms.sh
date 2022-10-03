68c50477bcc28b176c926f7321e53c424754be0b
Cortney Stedman <csted001@fiu.edu>
2022-10-03 15:48:19 -0400

    hw 1 complete

--- code/conversion/psb6351_convertdicoms.sh
+++ code/conversion/psb6351_convertdicoms.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #SBATCH -J psb6351_dcm_convert
-#SBATCH -o /scratch/classroom/psb6351/amattfel/crash/dcm_convert_out
-#SBATCH -e /scratch/classroom/psb6351/amattfel/crash/dcm_convert_err
+#SBATCH -o /scratch/classroom/psb6351/csted001/crash/dcm_convert_out
+#SBATCH -e /scratch/classroom/psb6351/csted001/crash/dcm_convert_err
 #SBATCH --qos pq_psb6351
 #SBATCH --account acc_psb6351
 #SBATCH --partition classroom
 
-heudiconv -d '/home/amattfel/Mattfeld_PSB6351/sourcedata/Mattfeld_REVL-000-vCAT-{subject}-S1/*/*/*/*/*/*' -s 021 -c none -f convertall -o /home/amattfel/Mattfeld_PSB6351/dset
+heudiconv -d '/home/csted001/Mattfeld_PSB6351/sourcedata/Mattfeld_REVL-000-vCAT-{subject}-S1/*/*/*/*/*/*' -s 021 -c dcm2niix -f /home/csted001/Mattfeld_PSB6351/code/conversion/psb6351_heuristic.py  -o /home/csted001/Mattfeld_PSB6351/dset
 
--- code/conversion/psb6351_heuristic.py
+++ code/conversion/psb6351_heuristic.py
@@ -4,7 +4,6 @@ def create_key(template, outtype=('nii.gz',), annotation_classes=None):
     if template is None or not template:
         raise ValueError('Template must be a valid format string')
     return template, outtype, annotation_classes
-
 def infotodict(seqinfo):
     """Heuristic evaluator for determining which runs belong where
 
@@ -19,41 +18,51 @@ def infotodict(seqinfo):
     bids_subject_session_prefix: BIDS subject/session prefix
     """
 
-    t1w = create_key('sub-{subject}/anat/sub-{subject}_run-{item}_T1w')
-    dwi = create_key('sub-{subject}/dwi/sub-{subject}_run-{item}_dwi')
-    loc1_task = create_key('')
-    loc2_task = create_key('')
-    study1_task = create_key('sub-{subject}/func/sub-{subject}_task-study_run-1_bold')
-    study2_task = create_key('')
-    study3_task = create_key('')
-    study4_task = create_key('')
-    task_fmap = create_key('')
-    dwi_fmap = create_key('')
+    t1w = create_key('sub-{subject}/ses-1/anat/sub-{subject}_run-{item}_T1w')
+    dwi = create_key('sub-{subject}/ses-1/dwi/sub-{subject}_run-{item}_dwi')
+    loc1_task = create_key('sub-{subject}/ses-1/func/sub-{subject}_loc_run-1_bold')
+    loc2_task = create_key('sub-{subject}/ses-1/func/sub-{subject}_loc_run-2_bold')
+    study1_task = create_key('sub-{subject}/ses-1/func/sub-{subject}_task-study_run-1_bold')
+    study2_task = create_key('sub-{subject}/ses-1/func/sub-{subject}_task-study_run-2_bold')
+    study3_task = create_key('sub-{subject}/ses-1/func/sub-{subject}_task-study_run-3_bold')
+    study4_task = create_key('sub-{subject}/ses-1/func/sub-{subject}_task-study_run-4_bold')
+    task_fmap = create_key('sub-{subject}/ses-1/fmap/sub-{subject}_task-study_run-1_fieldmap')
+    dwi_fmap = create_key('sub-{subject}/ses-1/fmap/sub-{subject}_task-study_run-1_b0')
 
     info = {t1w : [],
             dwi : [],
             loc1_task : [],
+            loc2_task : [],
             study1_task : [],
+            study2_task : [],
+            study3_task : [],
+            study4_task : [],
             task_fmap : [],
             dwi_fmap : []}
 
     for s in seqinfo:
         xdim, ydim, slice_num, timepoints = (s[6], s[7], s[8], s[9])
-        if (slice_num == ) and (timepoints == 1) and ("" in s.series_description):
+        if (slice_num == 176) and (timepoints == 1) and ("T1w_MPR_vNav" in s.series_description):
             info[t1w].append(s[2])
-        elif (slice_num > 1) and (timepoints == ) and ("" in s[12]):
+        elif (slice_num > 1) and (timepoints == 95) and ("dMRI_AP_REVL" in s.series_description):
             info[dwi].append(s[2])
-        elif (timepoints == ) and ("ROI_loc_1" in s[12]):
+        elif (timepoints == 304) and ("ROI_loc_1" in s.series_description):
             info[loc1_task].append(s[2])
-        elif (timepoints == ) and ("" in s[12]):
+        elif (timepoints == 303) and ("fMRI_REVL_ROI_loc_2" in s.series_description):
             info[loc2_task].append(s[2])
-        elif (timepoints == 355) and ('' in s[12]):
+        elif (timepoints == 355) and ('fMRI_REVL_Study_1' in s.series_description):
+            info[study1_task].append(s[2])
+        elif (timepoints == 351) and ('fMRI_REVL_Study_2' in s.series_description):
+            info[study1_task].append(s[2])
+        elif (timepoints == 350) and ('fMRI_REVL_Study_3' in s.series_description):
+            info[study1_task].append(s[2])
+        elif (timepoints == 345) and ('fMRI_REVL_Study_4' in s.series_description):
             info[study1_task].append(s[2])
         elif "dMRI_DistortionMap_AP" in s.series_description:
             info[dwi_fmap].append({"item": s[2], "dir": "AP"})
-        elif "" in s.series_description:
+        elif "dMRI_DistortionMap_PA" in s.series_description:
             info[dwi_fmap].append({"item": s[2], "dir": "PA"})
-        elif "" in s.series_description:
+        elif "fMRI_DistortionMap_PA" in s.series_description:
             info[task_fmap].append({"item": s[2], "dir": "PA"})
         elif "fMRI_DistortionMap_AP" in s.series_description:
             info[task_fmap].append({"item": s[2], "dir": "AP"})
